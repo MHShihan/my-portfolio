205e5712685960a900accd27391d1b2417506faf
MHShihan <mahmodul.no.1@gmail.com>
2024-03-02 23:10:25 +0600

    Added navbar initially

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,27 @@
 const Navbar = () => {
   return (
-    <div className="navbar bg-transparent text-white">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Link</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 rounded-t-none">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
+    <div className=" bg-transparent text-white">
+      <div className="navbar max-w-[1366px] mx-auto">
+        <a className="flex-1  items-center" href="home">
+          <img className="rounded-full" src="/src/assets/logo.png" alt="" />
+          <span className="pl-1 text-2xl font-bold">Dev Shihan</span>
+        </a>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 text-lg font-medium">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/particleConfig/ParticleConfig.js
+++ src/components/particleConfig/ParticleConfig.js
@@ -7,7 +7,7 @@ const ParticleConfig = {
         mode: "push",
       },
       onHover: {
-        enable: true,
+        enable: false,
         mode: "repulse",
       },
       resize: true,
@@ -17,7 +17,7 @@ const ParticleConfig = {
         quantity: 4,
       },
       repulse: {
-        distance: 200,
+        distance: 100,
         duration: 0.4,
       },
     },
@@ -30,7 +30,7 @@ const ParticleConfig = {
       color: "#ffffff",
       distance: 150,
       enable: true,
-      opacity: 0.5,
+      opacity: 0.3,
       width: 1,
     },
     move: {
@@ -40,7 +40,7 @@ const ParticleConfig = {
         default: "bounce",
       },
       random: false,
-      speed: 6,
+      speed: 0.3,
       straight: false,
     },
     number: {
@@ -48,16 +48,16 @@ const ParticleConfig = {
         enable: true,
         area: 800,
       },
-      value: 80,
+      value: 100,
     },
     opacity: {
-      value: 0.5,
+      value: 0.4,
     },
     shape: {
       type: "circle",
     },
     size: {
-      value: { min: 1, max: 5 },
+      value: { min: 1, max: 6 },
     },
   },
   detectRetina: true,
