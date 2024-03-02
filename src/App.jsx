c351053f1c1aa818c47ded55e3b69d61bd955438
MHShihan <mahmodul.no.1@gmail.com>
2024-03-02 22:06:16 +0600

    Added react particle js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useCallback } from "react";
+import Particles from "react-particles";
+//import { loadFull } from "tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+import Navbar from "./components/navbar/Navbar";
+import ParticleConfig from "./components/particleConfig/ParticleConfig";
+
 function App() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
-    <>
-      <h1 className="text-5xl text-center font-bold">Vite + React</h1>
-    </>
+    <div className="bg-[#1f1f1f]">
+      <Navbar />
+      <main className="snap-mandatory snap-y text-white">
+        <section className="snap-center">
+          <h1>Hero</h1>
+        </section>
+        <section className="snap-center">About</section>
+        <section className="snap-center">Project</section>
+        <section className="snap-center">Contact</section>
+      </main>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={ParticleConfig}
+      />
+    </div>
   );
 }
 
